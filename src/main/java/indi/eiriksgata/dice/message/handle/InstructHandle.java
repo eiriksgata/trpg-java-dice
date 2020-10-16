7fb55c7cf7705d5377ed6dbb81076159f7c15b06
Keith404 <2353686862@qq.com>
2020-10-16 16:48:44 +0800

    add new dice instruct class handle throws exception and separation instruction check function.

--- src/main/java/indi/eiriksgata/dice/message/handle/InstructHandle.java
+++ src/main/java/indi/eiriksgata/dice/message/handle/InstructHandle.java
@@ -1,8 +1,9 @@
 package indi.eiriksgata.dice.message.handle;
 
+import indi.eiriksgata.dice.exception.DiceInstructException;
+import indi.eiriksgata.dice.exception.ExceptionEnum;
 import indi.eiriksgata.dice.injection.InstructService;
 import indi.eiriksgata.dice.injection.InstructReflex;
-import indi.eiriksgata.dice.instuction.DiceInstructions;
 import org.reflections.Reflections;
 
 import java.lang.reflect.Method;
@@ -17,11 +18,16 @@ import java.util.Set;
 
 public class InstructHandle {
 
-    public String instructCheck(String message) {
-        return trackInstructCases(message);
+    //唯一公开的调用方法
+    public String instructCheck(String message) throws DiceInstructException {
+        //先行判断是否符合指令样式
+        if (message.substring(0, 1).equals(".")) {
+            return trackInstructCases(message);
+        }
+        throw new DiceInstructException(ExceptionEnum.DICE_INSTRUCT_NOT_FOUND);
     }
 
-    private String trackInstructCases(String message) {
+    private String trackInstructCases(String message) throws DiceInstructException {
         Reflections reflections = new Reflections("indi.eiriksgata.dice");
         Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(InstructService.class);
         for (Class clazz : typesAnnotatedWith) {
@@ -31,19 +37,28 @@ public class InstructHandle {
                     InstructReflex instructReflex = method.getAnnotation(InstructReflex.class);
                     String[] instructArr = instructReflex.value();
                     for (String temp : instructArr) {
-                        if (message.contains(temp)) {
+                        //方法增强检测命令
+                        if (instructMessageCheck(message, temp)) {
                             try {
                                 return (String) method.invoke(clazz.newInstance(), message);
                             } catch (Exception e) {
                                 e.printStackTrace();
-                                return "识别指令中出现了错误";
+                                throw new DiceInstructException(ExceptionEnum.DICE_INSTRUCT_HANDLE_ERR);
                             }
                         }
                     }
                 }
             }
         }
-        return null;
+        throw new DiceInstructException(ExceptionEnum.DICE_INSTRUCT_NOT_FOUND);
+    }
+
+
+    //仔细的指令检测方法
+    private boolean instructMessageCheck(String message, String instructStr) {
+        //具体的逻辑判定 该检测主要作为 统一的指令检测 若单条指令需要作检测，请在@InstrctionReflex的方法中实现
+
+        return message.contains(instructStr);
     }
 
 

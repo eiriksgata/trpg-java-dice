ffc192004b998d2be9e1f8341680edc88640b0d7
Keith404 <2353686862@qq.com>
2020-10-20 17:34:20 +0800

    add more instructions function impl.

--- src/main/java/indi/eiriksgata/dice/instruction/DiceInstructions.java
+++ src/main/java/indi/eiriksgata/dice/instruction/DiceInstructions.java
@@ -1,7 +1,14 @@
 package indi.eiriksgata.dice.instruction;
 
+import indi.eiriksgata.dice.vo.MessageData;
+import indi.eiriksgata.dice.config.DiceConfig;
+import indi.eiriksgata.dice.exception.DiceInstructException;
+import indi.eiriksgata.dice.exception.ExceptionEnum;
 import indi.eiriksgata.dice.injection.InstructService;
 import indi.eiriksgata.dice.injection.InstructReflex;
+import indi.eiriksgata.dice.operation.DiceSet;
+import indi.eiriksgata.dice.operation.RollBasics;
+import indi.eiriksgata.dice.reply.CustomText;
 
 /**
  * @author: create by Keith
@@ -12,15 +19,31 @@ import indi.eiriksgata.dice.injection.InstructReflex;
 @InstructService
 public class DiceInstructions {
 
+
     @InstructReflex(value = {".ra", ".rc"})
-    public String attributeCheck(String message) {
+    public String attributeCheck(MessageData data) {
         return "you input .ra";
     }
 
 
-    @InstructReflex(value = {".rd"})
-    public String rdCheck(String message) {
-        return "you input .rd";
+    @InstructReflex(value = {".r"})
+    public String roll(MessageData data) {
+        return new RollBasics().rollRandom(data.getMessage(), data.getQqID());
     }
+
+
+    @InstructReflex(value = {".MessageData", ".set"})
+    public String setDiceFace(MessageData data) throws DiceInstructException {
+        int setDiceFace = Integer.valueOf(data.getMessage());
+        if (setDiceFace > Integer.valueOf(DiceConfig.diceSet.getString("dice.face.max"))) {
+            throw new DiceInstructException(ExceptionEnum.DICE_SET_FACE_MAX_ERR);
+        }
+        if (setDiceFace <= Integer.valueOf(DiceConfig.diceSet.getString("dice.face.min"))) {
+            throw new DiceInstructException(ExceptionEnum.DICE_SET_FACE_MIN_ERR);
+        }
+        new DiceSet().setDiceSet(data.getQqID(), setDiceFace);
+        return CustomText.getText("dice.set.face.success", setDiceFace);
+    }
+
 
 }

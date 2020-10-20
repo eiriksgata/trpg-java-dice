6efbfdbef3687602f172eeff52be2d2b7408ac31
Keith404 <2353686862@qq.com>
2020-10-20 17:33:49 +0800

    add message data base parameter entity class.

---
+++ src/main/java/indi/eiriksgata/dice/vo/MessageData.java
@@ -0,0 +1,17 @@
+package indi.eiriksgata.dice.vo;
+
+import lombok.Data;
+
+/**
+ * @author: create by Keith
+ * @version: v1.0
+ * @description: indi.eiriksgata.dice
+ * @date:2020/10/20
+ **/
+@Data
+public class MessageData {
+
+    private String message;
+    private Long qqID;
+
+}

9417ac1ab301114c7de521cebb7384f015f4577e
Keith404 <2353686862@qq.com>
2020-10-26 16:52:09 +0800

    add san check function callback interface.

---
+++ src/main/java/indi/eiriksgata/dice/callback/SanCheckCallback.java
@@ -0,0 +1,13 @@
+package indi.eiriksgata.dice.callback;
+
+/**
+ * @author: create by Keith
+ * @version: v1.0
+ * @description: indi.eiriksgata.dice.callback
+ * @date:2020/10/26
+ **/
+public interface SanCheckCallback {
+
+    void getResultData(String attribute);
+
+}

56682e5ce3bd7bbf7f29e4402220d44f0d6a93a8
Keith404 <2353686862@qq.com>
2020-10-28 11:23:59 +0800

    add check base callback interface.

---
+++ src/main/java/indi/eiriksgata/dice/callback/CheckBaseCallback.java
@@ -0,0 +1,14 @@
+package indi.eiriksgata.dice.callback;
+
+/**
+ * @author: create by Keith
+ * @version: v1.0
+ * @description: indi.eiriksgata.dice.callback
+ * @date:2020/10/26
+ **/
+public interface CheckBaseCallback {
+
+
+
+
+}

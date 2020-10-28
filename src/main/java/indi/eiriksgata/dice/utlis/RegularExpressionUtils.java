c377bdffc76179af63106efd4626ae9abd1eb4c6
Keith404 <2353686862@qq.com>
2020-10-28 11:22:43 +0800

    add annotation describe.

--- src/main/java/indi/eiriksgata/dice/utlis/RegularExpressionUtils.java
+++ src/main/java/indi/eiriksgata/dice/utlis/RegularExpressionUtils.java
@@ -1,8 +1,5 @@
 package indi.eiriksgata.dice.utlis;
 
-
-import com.sun.istack.internal.NotNull;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -13,14 +10,14 @@ public class RegularExpressionUtils {
     /**
      * 正则表达式返回内容
      *
-     * @param regex
-     * @param source
-     * @return
+     * @param regex 正则表达式
+     * @param source 源文本
+     * @return 匹配字段
      */
     public static List<String> getMatchers(String regex, String source) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(source);
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
         while (matcher.find()) {
             list.add(matcher.group());
         }
@@ -28,6 +25,12 @@ public class RegularExpressionUtils {
     }
 
 
+    /**
+     * 返回单个匹配数据
+     * @param regex 正则表达式
+     * @param source 源文本
+     * @return 匹配字段
+     */
     public static String getMatcher(String regex, String source) {
         if (source == null) return null;
         Pattern pattern = Pattern.compile(regex);

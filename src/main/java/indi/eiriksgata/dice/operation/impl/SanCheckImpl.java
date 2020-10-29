47e02c34a1cd5fb51ba4d2d148f87cb9084242cd
Keith404 <2353686862@qq.com>
2020-10-29 15:25:51 +0800

    update san check function return value, fix san check big fail miscalculation.

--- src/main/java/indi/eiriksgata/dice/operation/impl/SanCheckImpl.java
+++ src/main/java/indi/eiriksgata/dice/operation/impl/SanCheckImpl.java
@@ -1,10 +1,12 @@
 package indi.eiriksgata.dice.operation.impl;
 
 import indi.eiriksgata.dice.callback.SanCheckCallback;
+import indi.eiriksgata.dice.reply.CustomText;
 import indi.eiriksgata.dice.utlis.CalcUtil;
 import indi.eiriksgata.dice.utlis.RegularExpressionUtils;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author: create by Keith
@@ -16,7 +18,7 @@ public class SanCheckImpl {
 
 
     //本方法需要采取回调的形式使用
-    public void sanCheck(String text, String attribute, SanCheckCallback callback) {
+    public String sanCheck(String text, String attribute, SanCheckCallback callback) {
 
         //筛选出检测属性 san
         String sanAttribute = RegularExpressionUtils.getMatcher("san[0-9]+", attribute);
@@ -30,44 +32,50 @@ public class SanCheckImpl {
         //检测成功
         if (random <= sanNumber) {
             //理智判定默认百面骰
-            new RollBasicsImpl().rollRandom(formula[0], 0L, (value, calculationProcess) -> {
-                int surplus = sanNumber - value;
+            AtomicInteger surplus = new AtomicInteger();
+            String resultText = new RollBasicsImpl().rollRandom(formula[0], 0L, (value, calculationProcess) -> {
+                surplus.set(sanNumber - value);
                 String resultAttribute = attribute.replaceFirst(sanAttribute, "san" + surplus);
-                callback.getResultData(resultAttribute, random, sanNumber, calculationProcess, surplus);
+                callback.getResultData(resultAttribute, random, sanNumber, calculationProcess, surplus.get());
 
             });
-            return;
+            return CustomText.getText("coc7.sc.success", text, random, sanNumber, resultText, surplus.get());
         }
 
         //检测大失败
         if (random == 100) {
             //计算大失败的数值
-            List<String> regexResult = RegularExpressionUtils.getMatchers("[0-9]?[Dd][0-9]?", formula[1]);
+            List<String> regexResult = RegularExpressionUtils.getMatchers("[0-9]?[Dd][0-9]+|[Dd]", formula[1]);
             for (String item : regexResult) {
                 if (item.substring(0, 1).equals("D") || item.substring(0, 1).equals("d")) {
                     if (item.length() == 1) {
-                        text = text.replaceFirst(item, "100");
+                        formula[1] = formula[1].replaceFirst(item, "100");
                     } else {
-                        text = text.replaceFirst(item, item.substring(1));
+                        formula[1] = formula[1].replaceFirst(item, item.substring(1));
                     }
                 } else {
-                    String itemValue = RegularExpressionUtils.getMatcher("[Dd][0-9]+", item);
-                    text = text.replaceFirst(item, itemValue.substring(1));
+                    //String itemValue = RegularExpressionUtils.getMatcher("[Dd][0-9]+", item);
+                    formula[1] = formula[1].replaceFirst("[dD]", "*");
                 }
             }
-            String formulaResult = new CalcUtil(text).toString();
+            String formulaResult = new CalcUtil(formula[1]).getResult().toString();
             String calProcess = text + "=" + formulaResult;
             int surplus = sanNumber - Integer.valueOf(formulaResult);
             String resultAttribute = attribute.replaceFirst(sanAttribute, "san" + surplus);
             callback.getResultData(resultAttribute, random, sanNumber, calProcess, surplus);
+            return CustomText.getText("coc7.sc.big-fail", text, random, sanNumber, calProcess, surplus);
+
+
         } else {
             //理智判定默认百面骰
-            new RollBasicsImpl().rollRandom(formula[1], 0L, (value, calculationProcess) -> {
-                int surplus = sanNumber - value;
+            AtomicInteger surplus = new AtomicInteger();
+            String resultText = new RollBasicsImpl().rollRandom(formula[1], 0L, (value, calculationProcess) -> {
+                surplus.set(sanNumber - value);
                 String resultAttribute = attribute.replaceFirst(sanAttribute, "san" + surplus);
-                callback.getResultData(resultAttribute, random, sanNumber, calculationProcess, surplus);
+                callback.getResultData(resultAttribute, random, sanNumber, calculationProcess, surplus.get());
 
             });
+            return CustomText.getText("coc7.sc.fail", text, random, sanNumber, resultText, surplus.get());
         }
 
 

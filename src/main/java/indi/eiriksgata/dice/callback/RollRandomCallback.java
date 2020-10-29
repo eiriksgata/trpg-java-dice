7e2557f774f0928d4053c0fb4c3eac7ea742a4b2
Keith404 <2353686862@qq.com>
2020-10-29 13:16:50 +0800

    add roll random function callback interface.

---
+++ src/main/java/indi/eiriksgata/dice/callback/RollRandomCallback.java
@@ -0,0 +1,13 @@
+package indi.eiriksgata.dice.callback;
+
+/**
+ * @author: create by Keith
+ * @version: v1.0
+ * @description: indi.eiriksgata.dice.callback
+ * @date:2020/10/29
+ **/
+public interface RollRandomCallback {
+
+    void getFormulaResult(int value, String calculationProcess);
+
+}

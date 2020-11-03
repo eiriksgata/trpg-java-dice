142707278d567c58a86a6786ef4bfbfde61a575b
Keith404 <2353686862@qq.com>
2020-11-03 10:12:16 +0800

    update code format,fix '。' instruct unable to identify the problem.

--- src/main/java/indi/eiriksgata/dice/message/handle/InstructHandle.java
+++ src/main/java/indi/eiriksgata/dice/message/handle/InstructHandle.java
@@ -20,20 +20,21 @@ import java.util.Set;
 
 public class InstructHandle {
 
+    private static Reflections reflections = new Reflections("indi.eiriksgata");
+
     //唯一公开的调用方法
     public String instructCheck(MessageData data) throws DiceInstructException, IllegalAccessException, InstantiationException, InvocationTargetException {
 
         //先行判断是否符合指令样式
-        if (data.getMessage().substring(0, 1).equals(".")) {
-            //将所有空格舍去
-            data.setMessage(data.getMessage().replaceAll(" ", ""));
+        if (data.getMessage().substring(0, 1).equals(".") || data.getMessage().substring(0, 1).equals("。")) {
+            //将所有空格舍去 移除空格应该在指令方法中去实现，这里不再作统一处理
+            //data.setMessage(data.getMessage().replaceAll(" ", ""));
             return trackInstructCases(data);
         }
         throw new DiceInstructException(ExceptionEnum.DICE_INSTRUCT_NOT_FOUND);
     }
 
     private String trackInstructCases(MessageData data) throws DiceInstructException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        Reflections reflections = new Reflections("indi.eiriksgata");
         Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(InstructService.class);
         Method highestPriority = null;
         Class highestClazz = null;
@@ -42,7 +43,6 @@ public class InstructHandle {
             Method[] methods = clazz.getDeclaredMethods();
             for (Method method : methods) {
                 if (method.isAnnotationPresent(InstructReflex.class)) {
-
                     String[] instructArr = method.getAnnotation(InstructReflex.class).value();
                     for (String temp : instructArr) {
                         //方法增强检测命令

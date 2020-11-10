38e1a0f30b3541acdc0070c0d6d7d75f9a7987c4
Keith404 <2353686862@qq.com>
2020-11-10 11:34:31 +0800

    add create random array list callback interface.

---
+++ src/main/java/indi/eiriksgata/dice/operation/impl/RollArrayCallback.java
@@ -0,0 +1,12 @@
+package indi.eiriksgata.dice.operation.impl;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.dice.operation.impl
+ * date: 2020/11/9
+ **/
+public interface RollArrayCallback {
+
+    void getResultData(int checkValue, int resultValue, int[] randomArr);
+}

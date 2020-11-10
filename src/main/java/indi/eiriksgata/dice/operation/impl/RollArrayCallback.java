8c70063a0df03baa3ce7cfa7df5e41ace1cb34f2
Keith404 <2353686862@qq.com>
2020-11-10 15:03:42 +0800

    update callback interface parameter.

--- src/main/java/indi/eiriksgata/dice/operation/impl/RollArrayCallback.java
+++ src/main/java/indi/eiriksgata/dice/operation/impl/RollArrayCallback.java
@@ -8,5 +8,5 @@ package indi.eiriksgata.dice.operation.impl;
  **/
 public interface RollArrayCallback {
 
-    void getResultData(int checkValue, int resultValue, int[] randomArr);
+    void getResultData(int checkValue, int[] sortArr, int[] randomArr);
 }

7d9b0fcec78345867589efd39e53b396798ea2d4
Eiriksgata <2353686862@qq.com>
2023-10-25 09:34:13 +0800

    rename class name

---
+++ src/main/java/com/github/eiriksgata/trpg/dice/vo/Message.java
@@ -0,0 +1,13 @@
+package com.github.eiriksgata.trpg.dice.vo;
+
+public abstract class Message {
+    String body ;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
